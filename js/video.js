a358b22613bd01e903b787253fcde4b622597d58
zenaTechfree <yeahwonzena@techfree.kr>
2025-05-12 01:53:35 +0900

    Edit: env사용으로 변경

--- js/video.js
+++ js/video.js
@@ -36,8 +36,7 @@ async function getSimilarity(firstWord, secondWord) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        // Authorization: process.env.API_KEY,
-        Authorization: "84fc9029-2ecd-477e-9c8e-ffa929e32c2b"
+        Authorization: process.env.API_KEY,
       },
       body: JSON.stringify(requestJson),
     });
